27d4df73677c3e0307313e4fcd710b8a541bb657
Matthew Hallberg <matthewhallberg@matthews-mbp-2.lan>
2021-04-04 22:44:31 -0400

    scary face working.

---
+++ Public/DemonFace.js
@@ -0,0 +1,34 @@
+//@input Component.FaceStretchVisual faceStretch
+//@input Component.FaceMaskVisual faceMask
+//@input Component.RenderMeshVisual mouth
+//@input Component.LiquifyVisual liquify
+
+script.createEvent("UpdateEvent").bind(Update);
+script.createEvent("MouthOpenedEvent").bind(MouthOpened);
+script.createEvent("MouthClosedEvent").bind(MouthClosed);
+
+var desiredIntensity = 0;
+var speed = 4;
+
+function MouthOpened(){
+    desiredIntensity = 1;
+    desiredAlpha = 1;
+}
+
+function MouthClosed(){
+    desiredIntensity = 0;
+    desiredAlpha = 0;
+}
+
+function Update(){
+    var currIntensity = script.faceStretch.getFeatureWeight("Feature0");
+    var intensity = lerp(currIntensity,desiredIntensity, getDeltaTime() * speed);
+    script.faceStretch.setFeatureWeight("Feature0", intensity);
+    script.faceMask.mainMaterial.mainPass.baseColor = new vec4(1, 1, 1, intensity);
+    script.mouth.mainMaterial.mainPass.baseColor = new vec4(.15, .15, .15, intensity);
+    script.liquify.radius = intensity * 4;
+}
+
+function lerp(start, end, t){
+    return start * (1 - t) + end * t;
+}
